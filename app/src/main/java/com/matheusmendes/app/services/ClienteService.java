239c2ad5aa9ce5cefc89716b67119dde39101674
Matheus Mendes <matheusmendesreis@outlook.com>
2025-07-18 13:38:23 -0300

    adiciona anotação      @Transactional(readOnly = true)

--- app/src/main/java/com/matheusmendes/app/services/ClienteService.java
+++ app/src/main/java/com/matheusmendes/app/services/ClienteService.java
@@ -6,16 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.matheusmendes.app.dto.ClienteDTO;
 import com.matheusmendes.app.entities.Client;
 import com.matheusmendes.app.repositories.ClientRepostory;
+
 
 @Service
 public class ClienteService {
     @Autowired
     private ClientRepostory repostory;
 
+    @Transactional(readOnly = true)
     public Page<ClienteDTO> findAll(Pageable p) {
         Page<Client> clients = repostory.findAll(p);
         return clients.map(c -> new ClienteDTO(c.getId(), c.getName(), c.getCpf(), c.getIncome(),c.getBirthDate(), c.getChildren()));
