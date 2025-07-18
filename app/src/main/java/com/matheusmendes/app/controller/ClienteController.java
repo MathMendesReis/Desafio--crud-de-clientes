1f1c521198efff9eec637b633ebd5ad7b7a74b13
Matheus Mendes <matheusmendesreis@outlook.com>
2025-07-18 15:31:38 -0300

    inserção de cliente

--- app/src/main/java/com/matheusmendes/app/controller/ClienteController.java
+++ app/src/main/java/com/matheusmendes/app/controller/ClienteController.java
@@ -2,9 +2,14 @@ package com.matheusmendes.app.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheusmendes.app.dto.ClienteDTO;
 import com.matheusmendes.app.services.ClienteService;
+
+import jakarta.validation.Valid;
+
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "clients")
@@ -30,4 +37,12 @@ public class ClienteController {
         ClienteDTO result = service.findById(id);
         return ResponseEntity.ok().body(result);
     }
+
+    @PostMapping()
+    public ResponseEntity<ClienteDTO> save(@Valid @RequestBody ClienteDTO entity) {
+        ClienteDTO result = service.save(entity);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(result.getId()).toUri();
+        return ResponseEntity.created(uri).body(result);
+    }
+
 }
